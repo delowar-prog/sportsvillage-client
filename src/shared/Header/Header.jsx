d3a7de3c4ff5a1256b56d55b1b958c573268742d
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-18 19:06:49 +0600

    footer section done

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import Logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../providers/AuthProviders'
+import Profile from '../../assets/profile.jpg'
 const Header = () => {
+  const {user}=useContext(AuthContext)
   return (
     <div className='sticky top-0 z-50'>
       <div className="navbar flex flex-col sm:flex-row bg-indigo-800  px-5 lg:px-14">
@@ -44,13 +48,11 @@ const Header = () => {
           <div className="form-control">
             <input type="text" placeholder="Search" className="input" />
           </div>
-          <div>
-            <Link to='/login' className='text-white'>login</Link>
-          </div>
-          <div className="dropdown dropdown-end">
+          {
+            user ? <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <img src={Profile} />
               </div>
             </label>
             <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
@@ -63,7 +65,13 @@ const Header = () => {
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
             </ul>
-          </div>
+          </div>:
+          <div>
+          <Link to='/login' className='text-white'>login</Link>
+        </div>
+          }
+          
+          
         </div>
       </div>
     </div>
